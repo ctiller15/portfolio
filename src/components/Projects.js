d67f57d72e5cfa35af538bf7ee79879884c10702
ctiller15 <christiller2015@gmail.com>
2017-11-03 15:17:07 -0400

    Grab project images, tweak projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,28 +9,19 @@ class Projects extends React.Component{
 		return(
 			<div className="projectSect">
 				<div className="projectNav">
-					<button>Professional</button>
 					<h2>Projects</h2>
-					<button>Personal</button>
 				</div>
 				<div className="projectDisplay">
 					{/*iframe goes here!*/}
-					<iframe className="projectView"></iframe>
+					<iframe className="projectView" src="https://floating-atoll-98238.herokuapp.com/" frameBorder="0"></iframe>
 					<div className="projectInfo">
 						<p>This is some dummy text describing the project</p>
 					</div>
 				</div>
 				<div className="professional projectList">
-					<div>Project 1</div>
-					<div>Project 2</div>
-					<div>Project 3</div>
-					<div>Project 4</div>
-					<div>Project 5</div>
-					<div>Project 6</div>
-				</div>
-				
-				<div className="personal projectList hiddenProj">
-					<div>Project 1</div>
+					<div>
+						<iframe src="https://shielded-coast-36823.herokuapp.com/"></iframe>
+					</div>
 					<div>Project 2</div>
 					<div>Project 3</div>
 					<div>Project 4</div>
