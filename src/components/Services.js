2fb444a559412c225df387923ea307ba16c1b6ca
ctiller15 <christiller2015@gmail.com>
2017-11-10 10:53:00 -0500

    update tablet view

--- src/components/Services.js
+++ src/components/Services.js
@@ -12,34 +12,36 @@ class Services extends React.Component{
 
 				<p>When I build a website for you, you get...</p>
 
-				<div className="promise1">
-					<h3>Clear communication</h3>
-					<div>
-						{/*<i class="fa fa-bullhorn" aria-hidden="true"></i>*/}
-						{/*http://simpleicon.com/wp-content/uploads/loudspeaker.svg*/}
-						<img src="/svg/loudspeaker.svg"/>
+				<div className="promiseContainer">
+					<div className="promise1">
+						<h3>Clear communication</h3>
+						<div>
+							{/*<i class="fa fa-bullhorn" aria-hidden="true"></i>*/}
+							{/*http://simpleicon.com/wp-content/uploads/loudspeaker.svg*/}
+							<img src="/svg/loudspeaker.svg"/>
+						</div>
+						<p>Clean, clear design ensures that your users know exactly what you offer.</p>
 					</div>
-					<p>Clean, clear design ensures that your users know exactly what you offer.</p>
-				</div>
 
-				<div className="promise2">
-					<h3>Responsive design</h3>
-					<div>
-						{/*http://simpleicon.com/responsive-design.html
-						No clue who made this, find out?
-						*/}
-						<img src="/svg/responsive-design.svg"/>
+					<div className="promise2">
+						<h3>Responsive design</h3>
+						<div>
+							{/*http://simpleicon.com/responsive-design.html
+							No clue who made this, find out?
+							*/}
+							<img src="/svg/responsive-design.svg"/>
+						</div>
+						<p>Your website will be optimized for everything from the smallest phones to the largest desktops</p>
 					</div>
-					<p>Your website will be optimized for everything from the smallest phones to the largest desktops</p>
-				</div>
 
-				<div className="promise3">
-					<h3>Easy support and management</h3>
-					<div>
-						{/*<a href="http://www.onlinewebfonts.com">oNline Web Fonts</a>*/}
-						<img src="/svg/maintenance.svg"/>
+					<div className="promise3">
+						<h3>Easy support and management</h3>
+						<div>
+							{/*<a href="http://www.onlinewebfonts.com">oNline Web Fonts</a>*/}
+							<img src="/svg/maintenance.svg"/>
+						</div>
+						<p>I don't vanish after your site is built. I give you the means to handle the simple stuff, and if something goes wrong? Just give me a call.</p>
 					</div>
-					<p>I don't vanish after your site is built. I give you the means to handle the simple stuff, and if something goes wrong? Just give me a call.</p>
 				</div>
 
 				<div className="contactButtons">
