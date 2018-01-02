fb6bf0277aa2109bb3ba5148fb79b8494d3dbc64
ctiller15 <christiller2015@gmail.com>
2018-01-02 13:06:37 -0500

    update navbar with transitions

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,6 @@ class Navbar extends React.Component{
 		var scrollNav = document.querySelector("nav");
 		var stickPos = window.pageYOffset + scrollNav.getBoundingClientRect().y;
 		var stickPosHeight = scrollNav.getBoundingClientRect().height;
-		// console.log(stickPos);
 		var beforeElement = document.querySelector('.banner');
 		var style = window.getComputedStyle(beforeElement);
 		var marginBottom = style.getPropertyValue('margin-bottom');
@@ -22,19 +21,12 @@ class Navbar extends React.Component{
 	}
 
 	handleScroll(pos, height, elem, marg){
-		console.log(pos);
-		// console.log(window.pageYOffset);
-		// console.log(pos);
-		// console.log(typeof(marg));
 
 		if(window.pageYOffset > pos){
-			// console.log("We stickin' now!");
 			elem.style.position = "fixed";
 			elem.style.top = "0";
-		{/*Remember, these values are only temporary! Code with more variable values later!*/}
 			document.querySelector(".banner").style.marginBottom = `${marg + height}px`;
 		} else {
-			// console.log("Not sticky enough...");
 			elem.style.position = "relative";
 			elem.style.top = "null";
 			document.querySelector(".banner").style.marginBottom = `${marg}px`;
@@ -45,7 +37,10 @@ class Navbar extends React.Component{
 		return(
 			<nav>
 				<div className="Navbar">
-						<span className="main"><a href="#banner">CT</a> </span>
+						<span className="main"> 
+							<a href="https://github.com/ctiller15" target="_blank" rel="noopener noreferrer"><i className="fa fa-github-square" aria-hidden="true"></i></a>
+							<a href="https://www.linkedin.com/in/christopher-tiller-9b853b129/" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a> 
+						</span>
 						<span><a href="#about">About</a> </span>
 						<span><a href="#projects">Projects</a> </span>
 						<span><a href="#contact">Contact</a> </span>
